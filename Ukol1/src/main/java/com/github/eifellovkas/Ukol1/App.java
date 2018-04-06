811a4c191cce0dde0c1cb7971086013b55f3fbd2
plesk <pleskacv@gmail.com>
2018-04-06 22:11:14 +0200

    first

---
+++ Ukol1/src/main/java/com/github/eifellovkas/Ukol1/App.java
@@ -0,0 +1,13 @@
+package com.github.eifellovkas.Ukol1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
